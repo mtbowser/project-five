e265b51f14ba4378049f8c397460c418ef0942dc
Maria Bowser <mariatbowser@vt.edu>
2018-04-05 19:25:07 -0400

    hi

--- src/prj5/Input.java
+++ src/prj5/Input.java
@@ -1,10 +1,18 @@
 package prj5;
+
+import java.util.Scanner;
+
 public class Input
 {
     //~ Fields ................................................................
-
+    Scanner studentScan;
+    Scanner songScan;
     //~ Constructors ..........................................................
-
+    public Input() 
+    {
+        
+        
+    }
     //~Public  Methods ........................................................
 
 }
